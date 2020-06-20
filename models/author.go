244c1501bb22dc2a2748f94880507b60c41dcc59
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2020-06-20 23:17:35 +0530

    add: author list type alias for author slice add: isDuplicate method in author list type

--- models/author.go
+++ models/author.go
@@ -4,3 +4,15 @@ type Author struct {
   Id uint32
   Name string
 }
+
+type AuthorList []Author
+
+func (authors AuthorList) IsDuplicate(author Author) bool {
+  for _, a := range authors {
+    if a.Id == author.Id || a.Name == author.Name {
+      return true
+    }
+  }
+
+  return false
+}
