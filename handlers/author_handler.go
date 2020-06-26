0cd7cd294e9d4cad80272d0eb46a3336233bf5f4
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2020-06-26 21:44:14 +0530

    add: end point for get author by id

--- handlers/author_handler.go
+++ handlers/author_handler.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
-	repo "github.com/sm3saurabh/book_store/repo/author"
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"github.com/gorilla/mux"
+	repo "github.com/sm3saurabh/book_store/repo/author"
 )
 
 var authorRepo repo.AuthorRepository
@@ -13,6 +14,30 @@ func HandleAuthorRequests(router *mux.Router) {
   authorRepo = repo.NewInMemoryAuthorRepository()
 
   router.HandleFunc("/authors", getAuthors).Methods("GET")
+  router.HandleFunc("/author/{id}", getAuthorById).Methods("GET")
+}
+
+func getAuthorById(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+
+  if id, ok := params["id"]; ok {
+
+    actualId, nfe := strconv.ParseUint(id, 10, 32)
+
+    if nfe != nil {
+      http.Error(w, "Unsupported id format", http.StatusBadRequest)
+      return
+    }
+
+    author, err := authorRepo.GetAuthorById(uint32(actualId))
+
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+
+    json.NewEncoder(w).Encode(author)
+  }
 }
 
 func getAuthors(w http.ResponseWriter, r *http.Request) {
